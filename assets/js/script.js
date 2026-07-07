6bb293e96e141ca9f634de8eced4417ea915cf04
Ali Dehbashi <alid3@illinois.edu>
2026-07-08 01:35:25 +0330

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+fetch("credentials.json")
+  .then(response => response.json())
+  .then(data => {
 
+    const container = document.getElementById("credentials");
+
+    data.forEach(item => {
+
+      container.innerHTML += `
+        <div class="card">
+
+            <img src="${item.logo}" alt="${item.issuer}">
+
+            <h2>${item.title}</h2>
+
+            <p><strong>Issuer:</strong> ${item.issuer}</p>
+
+            <p><strong>Issued:</strong> ${item.issued}</p>
+
+            <a class="button" href="${item.pdf}" target="_blank">
+                View Certificate
+            </a>
+
+        </div>
+      `;
+
+    });
+
+  });
